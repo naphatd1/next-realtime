bf1447062564e192a72a4ed71473d7f46d380595
Naphat <naphat.d@gmail.com>
2024-12-04 10:18:53 +0700

    workflows

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'mysql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'mysql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
 });
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,8 +1,38 @@
-import { drizzle } from 'drizzle-orm/mysql2'
+import Typography from '@mui/material/Typography'
 import { user } from '../db/schema'
+import db from '../lib/db'
+import { Box } from '@mui/material'
+import { desc } from 'drizzle-orm'
 
 export default async function UserPage() {
-  const db = drizzle(process.env.DATABASE_URL!)
-  const users = await db.select().from(user)
-  return <>{JSON.stringify(users)}</>
+  const users = await db.select().from(user).orderBy(desc(user.id))
+  const countUser = await db.$count(user)
+  return (
+    <main>
+      <Typography sx={{ m: 1 }} variant="h5">
+        ผู้ใช้ทั้งหมด {countUser}
+      </Typography>
+      {users.length > 0 &&
+        users.map((item) => {
+          return (
+            <>
+              <Box
+                component="div"
+                sx={{
+                  ml: 1,
+                  color: 'primary.main',
+                  display: 'inline-flex',
+                  justifyContent: 'center',
+                  alignContent: 'center'
+                }}
+              >
+                <p key={item.id}>
+                  {item.fullname}:{item.id}
+                </p>
+              </Box>
+            </>
+          )
+        })}
+    </main>
+  )
 }
